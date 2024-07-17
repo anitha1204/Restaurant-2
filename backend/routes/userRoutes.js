a5815b5ff2fcd3a1f12fc2956204e638752cc171
anitha1204 <155633514+anitha1204@users.noreply.github.com>
2024-07-17 03:45:14 -0700

    Delete backend/routes directory

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {userpostData,usergetData} = require('../controllers/userController');
-
-router.post('/post', userpostData);
-router.get('/get', usergetData);
-
-module.exports = router;
