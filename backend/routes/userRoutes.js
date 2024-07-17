e7351ee9c492c332a919d2659e81872ef1eadfca
anitha1204 <155633514+anitha1204@users.noreply.github.com>
2024-07-17 16:16:25 +0530

    Add files via upload

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const router = express.Router();
+const { postUserData, getUserData } = require('../controllers/userController');
+
+router.post('/post', postUserData);
+router.get('/get/:email', getUserData);
+
+module.exports = router;
